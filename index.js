b434eea8e87255de49ecaa2f7fd4eb9a890051fb
legojrp <legojrp@gmail.com>
2023-01-23 23:27:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,8 @@ document.addEventListener("click",function(){
 })
 
 function isValidUrl(string) {
-    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    if (string){
+        var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    }
     return (res !== null)
   };
